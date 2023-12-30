7fc990b3f25d5608b576f5f98cda855a9026396b
Gabriel Silva <gabriel.silva_dev@outlook.com>
2023-12-30 05:12:34 -0300

    Update app.py

--- app.py
+++ app.py
@@ -12,10 +12,10 @@ texto = custom.CTkLabel(app, text="Link: ")
 texto.place(x=10, y=15)
 
 texto2 = custom.CTkLabel(app, text="Link gerado e já copiado: ")
-texto2.place(x=80, y=110)
+texto2.place(x=80, y=90)
 
-urll = custom.CTkEntry(app, placeholder_text="Link longo", width=240)
-urll.place(x=55, y=15)
+urll = custom.CTkEntry(app, placeholder_text="Link longo", width=230)
+urll.place(x=45, y=15)
 
 def ctrlC(text):
     app.clipboard_clear()
@@ -36,7 +36,7 @@ def click ():
             
             
             short = custom.CTkLabel(app, text=surl)
-            short.place(x=160, y=110)           
+            short.place(x=70, y=120)           
             
             ctrlC(surl)
     
@@ -47,7 +47,7 @@ def click ():
         messagebox.showerror('ERRO!', f'Erro ao gerar link: {str(e)}')
 
 btn = custom.CTkButton(app, text="Encurtar", command=click
-).place(x=80, y=65)
+).place(x=80, y=55)
 
 
 app.mainloop()
